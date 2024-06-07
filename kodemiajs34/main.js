02562c3da7a65fb94af0d4e9453f5adfa115523d
NeriGRx <neri.games.gr@gmail.com>
2024-06-06 20:48:00 -0600

    feat: se creo main.js y se empezo a trabajar en el

---
+++ kodemiajs34/main.js
@@ -0,0 +1,66 @@
+console.log("hola koders");
+
+/* delaclaracion*/
+let myName = "Gerardo Neri"
+
+/*Resignacion*/
+myName = "Naomi Salinas"
+
+let foo = "some value";
+
+console.log(foo);
+
+foo = "other value";
+
+console.log(foo);
+
+const BASE_URL = "my-user-api.com"
+
+console.log(BASE_URL); 
+
+let myGlobalVariable = "global value";
+
+console.log(myGlobalVariable);
+
+const myFuncion = () => {
+    console.log(myGlobalVariable);
+    let myLocalVariable = "my local value";
+    console.log(myLocalVariable);
+};
+
+myFuncion();
+
+let x = 27
+
+console.log(x);
+
+let y = (x);
+
+console.log(y);
+
+x = 72; 
+
+console.log(x);
+
+console.log(y);
+
+let kodersList = [
+    "Pedro", "Lau"];
+
+console.log(kodersList);
+
+let secondKoderList = kodersList;
+
+console.log(secondKoderList);
+
+kodersList.push("Rodrigo");
+
+console.log(secondKoderList);
+
+console.log(kodersList)
+
+secondKoderList.push("third koder");
+
+console.log(kodersList);
+
+console.log(secondKoderList);
