2fa09fc1f56c30ea425b16e07efe6c2d8c91b3e9
NeriGRx <neri.games.gr@gmail.com>
2024-06-11 22:05:09 -0600

    .

---
+++ s3ciclos/main.js
@@ -0,0 +1,20 @@
+let phrase = "No lo intentes. Hazlo, o no lo hagas, pero no lo intentes.";
+
+let firstChar = phrase[1];
+let secondChar = phrase[4];
+let thirdChar = phrase[8];
+let fourthChar = phrase[9];
+
+console.log(firstChar);
+console.log(secondChar);
+console.log(thirdChar);
+console.log(fourthChar);
+
+
+let phrase1 = "hola"
+for( let i = 0; i < phrase1.length; i++){
+ console.log("i:", i);
+ console.log('phrase1:', phrase1);
+ console.log("char: ", phrase1[i]);
+ console.log("---");
+}
